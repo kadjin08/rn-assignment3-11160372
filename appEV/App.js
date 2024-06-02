249b4c8fb7bf190be32add96f6bbc3828c08757d
kadjin08 <knaayitey-adjin@st.ug.edu.gh>
2024-06-02 19:22:43 +0000

    categories and cards done

--- appEV/App.js
+++ appEV/App.js
@@ -28,7 +28,64 @@ export default function App() {
         </View>
 
 
+      <View style={styles.content}>
 
+        <View>
+          <Text style={{ fontSize: 28, paddingTop: 40, fontWeight:'bold'}}>Categories</Text>
+        </View>
+        <View style={styles.generalContent}>
+
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+
+            <View style={{ flexDirection: 'row', paddingTop: 15 }}>
+
+              <View style={styles.card} backgroundColor="white">
+                <Text style={{ fontSize: 21, fontWeight:'bold'}}>Exercise</Text>
+                <Text>7 tasks</Text>
+                <Image source={require('./assets/young woman working 555.png')} style={{ padding: 0, marginHorizontal: 0 }} />
+              </View>
+
+              <View style={styles.card} backgroundColor="white">
+                <Text style={{ fontSize: 21, fontWeight:'bold' }}>Research</Text>
+                <Text>10 tasks</Text>
+                <Image source={require('./assets/young woman working1.1.png')} style={{ padding: 10, marginHorizontal: 20 }} />
+              </View>
+
+              <View style={styles.card} backgroundColor="white">
+                <Text style={{ fontSize: 21 , fontWeight:'bold'}}>Entertainment</Text>
+                <Text>8 tasks</Text>
+                <Image source={require('./assets/entertainment copy.jpg')} style={{ padding: 5, marginHorizontal: 5 }} />
+              </View>
+
+              <View style={styles.card} backgroundColor="white">
+                <Text style={{ fontSize: 21, fontWeight:'bold' }}>Art</Text>
+                <Text>5 tasks</Text>
+                <Image source={require('./assets/pottery art.png')} style={{ padding: 10, marginHorizontal: 20 }} />
+              </View>
+
+              <View style={styles.card} backgroundColor="white">
+                <Text style={{ fontSize: 21,fontWeight:'bold' }}>Work</Text>
+                <Text>15 tasks</Text>
+                <Image source={require('./assets/corporate work.png')} style={{ padding: 5, marginHorizontal: 50 , width:100}} />
+              </View>
+
+              <View style={styles.card} backgroundColor="white">
+                <Text style={{ fontSize: 21, fontWeight:'bold' }}>Virtual Reality</Text>
+                <Text>3 tasks</Text>
+                 <Image source={require('./assets/vr image.png')} style={{ padding: 10, marginHorizontal: 20 }} /> 
+              </View>
+
+              <View style={styles.card} backgroundColor="white">
+                <Text style={{ fontSize: 21,fontWeight:'bold' }}>Product testing</Text>
+                <Text>6 tasks</Text>
+                <Image source={require('./assets/product testing 27.png')} style={{ padding: 50, marginHorizontal: 20 }} />
+              </View>
+            </View>
+
+          </ScrollView>
+
+
+</View>
         </ScrollView>
   );
 }
