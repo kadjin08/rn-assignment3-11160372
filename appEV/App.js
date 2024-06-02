e887430e53c40bc16fb0be6b22e3fd0f2454e1ab
kadjin08 <knaayitey-adjin@st.ug.edu.gh>
2024-06-02 19:23:57 +0000

    ongoing tasks section done

--- appEV/App.js
+++ appEV/App.js
@@ -86,6 +86,156 @@ export default function App() {
 
 
 </View>
-        </ScrollView>
+
+</View>
+        <View>
+          <Text style={{ fontSize: 28, paddingTop: 40 , fontWeight:'bold'}}>Ongoing Task</Text>
+        </View>
+        <View style={{ flexDirection: 'column', paddingTop: 10 }}>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20, fontWeight:'bold' }}>Update school administration system</Text>
+          </View>
+
+          <View style={styles.task} >
+            <Text style={{ fontSize: 20, fontWeight:'bold' }}>Developing web app</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white' >
+            <Text style={{ fontSize: 20, fontWeight:'bold'}}>listening to music</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20,fontWeight:'bold' }}>Push Ups</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20,fontWeight: 'bold' }}>Make collage art</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20, fontWeight:'bold' }}>Make sculptures</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white' >
+            <Text style={{ fontSize: 20, fontWeight:'bold' }}>Watching movies</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20, fontWeight:'bold' }}>Doing yoga</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20, fontWeight:'bold' }}>Learning about AI</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20,fontWeight:'bold' }}>Testing new food delivery app</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20, fontWeight:'bold' }}>Push ups</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20,fontWeight:'bold' }}>Watching football</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20, fontWeight:'bold' }}>playing video games</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white' >
+            <Text style={{ fontSize: 20,fontWeight:'bold' }}>Machine learning</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20, fontWeight:'bold' }}>Researching the side effects of AI</Text>
+          </View>
+
+
+
+
+
+
+        </View>
+
+        </View>
+
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {paddingTop:76,
+                padding:30, 
+                backgroundColor: '#F7F0E8'
+                
+  
+  },
+
+  firsttab:{
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    
+  }
+,   searchRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: 30,
+  },
+
+  searchSection: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    height: 45,
+    width: 40,
+    padding: 5,
+  },
+
+  searchIcon: {
+    padding: 5,
+  },
+
+  input: {
+    width: 200,
+    fontSize: 16,
+    left: 10,
+    backgroundColor: 'white',
+   
+    
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    height: 200,
+    width: 250,
+    padding: 15,
+    marginRight: 20,
+
+  },
+
+  task: {
+    marginBottom: 20,
+    height: 100,
+    
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#E8D1BA',
+    padding: 15,
+    alignItems: 'left',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+
+  Image: {
+    height: 100,
+    width: 100,
+  }
+
+});
+
+ 
